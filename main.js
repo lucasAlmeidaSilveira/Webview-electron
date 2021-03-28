311036e64bb944e729dafd300e1079446a54236d
lucasAlmeidaSilveira <lucas.asilveira.sh@gmail.com>
2021-03-28 12:39:39 -0300

    :sparkles: App Finished

---
+++ main.js
@@ -0,0 +1,36 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+let win;
+
+function createWindow() {
+  win = new BrowserWindow({
+    width: 550,
+    height: 400,
+    // frame: false,
+    autoHideMenuBar: true,
+    alwaysOnTop: true,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+    },
+  });
+
+  win.loadURL("http://localhost:5500/");
+  /*   win.webContents.openDevTools() */
+}
+
+app.whenReady().then(createWindow)
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
+
+
